6c2a49d3b9e08adad9b34377ebc63af9e07d4693
Furkan Kümüş <furkankumus1905@gmail.com>
2023-07-26 16:25:41 +0300

    DbContext added

---
+++ MovieAppApi/Data/AngularExampleDbContext.cs
@@ -0,0 +1,18 @@
+﻿using AngularExampleAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AngularExampleAPI.Data
+{
+    public class AngularExampleDbContext :DbContext
+    {
+        public AngularExampleDbContext(DbContextOptions options) 
+            : base(options) { }
+        public DbSet<Urun> Urunler { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseNpgsql("Host=localhost; Database=AngularMovieApp; Username=postgres; Password=123; Port=5432");
+        }
+    }
+}
