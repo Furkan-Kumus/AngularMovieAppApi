eef52d6a3af50740cc5480fec87655d9a13715b6
Furkan Kümüş <furkankumus1905@gmail.com>
2023-07-26 16:27:00 +0300

    Models added

---
+++ MovieAppApi/Models/Urun.cs
@@ -0,0 +1,11 @@
+﻿namespace AngularExampleAPI.Models
+{
+    public class Urun
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string UrunId { get; set; }
+        public decimal Price { get; set; }
+    }
+}
